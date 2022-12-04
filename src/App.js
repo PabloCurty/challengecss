86cd37cbeb8b02229119c09149f0582249defd8c
Pablo Curty <pablo.curty@visagio.com>
2022-12-04 12:43:35 -0300

    challenge css completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Carros from './Components/Carros';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const carros = [
+    { id: 1, nome: "Uno", marca: "Fiat" },
+    { id: 2, nome: "Audi", marca: "Wolks" },
+    { id: 3, nome: "Gol", marca: "Wolks" },
+    { id: 4, nome: "Renegade", marca: "Jeep" },
+  ];
+  return (<div>
+    <h1>Challenge css</h1>
+    <h2>carros</h2>
+      {carros.map((carro) => (
+        <Carros id={carro.id} carro={carro.nome} marca={carro.marca} />
+      ))}
     </div>
   );
 }
